caae5228c21c96a725fc87580d28aa5d92227827
Haley Townsend <haleyrk@hotmail.com>
2018-01-19 14:03:43 -0600

    get endpoint

--- back-end/routes/migraines.js
+++ back-end/routes/migraines.js
@@ -21,12 +21,23 @@ router.post('/', (req, res) => {
     res.status(201).end()
   }
   catch (message) {
-    return res.status(400).end({error: true, message})
+    return res.status(400).json({error: true, message}).end()
   }
 })
 
-router.get('/:migraine-id', (req, res) => {
-  res.send('TODO: implement migraine view')
+
+// http://haleys-migraine-app.com/migraines/a1b2c3d4e5
+router.get('/:id', (req, res) => {
+  Migraine.findById(req.params['id']).exec()
+    .then(migraine => {
+      // if this function executes, Migraine found a valid migraine entry with
+      // that _id
+      res.status(200).json(migraine).end()
+    })
+    .catch(message => {
+      // if this function executes, Migraine didn't find and throw an error
+      res.status(404).json({ error: true, message }).end()
+    })
 })
 
 router.put('/:migraine-id', (req, res) => {
