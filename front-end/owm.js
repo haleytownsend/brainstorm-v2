942a135839fa1d0fcd9a32a9e9e080e612e38efa
Haley Townsend <haleyrk@hotmail.com>
2018-01-31 16:07:53 -0600

    styling

--- front-end/owm.js
+++ front-end/owm.js
@@ -41,20 +41,24 @@ owm.getForecast = function (options) {
     console.error('Something went wrong while requesting weather forecast', error);
   })
   .then(payload => {
+    //const day_min = payload.map(list => list.main.temp_min)
+    //var day_max = payload.map(migraine => migraine.createdAt.slice(6,10))
+    //console.log(day_min);
     console.log(payload);
-    $('#day1').html(payload.list["0"].dt_txt);
+    $('#day1').html(payload.list["0"].dt_txt.slice(5,10)+'-2018');
+    //$('#day1-temp').html(payload.list["0"].main.temp_min + '°' + ' || ' + payload.list["0"].main.temp_max + '°');
     $('#day1-temp').html(payload.list["0"].main.temp + '°');
     $('#day1-icon').html('<img src="http://openweathermap.org/img/w/'+ payload.list["0"].weather["0"].icon + '.png">');
-    $('#day2').html(payload.list["8"].dt_txt);
+    $('#day2').html(payload.list["8"].dt_txt.slice(5,10)+'-2018');
     $('#day2-temp').html(payload.list["8"].main.temp + '°');
     $('#day2-icon').html('<img src="http://openweathermap.org/img/w/'+ payload.list["8"].weather["0"].icon + '.png">');
-    $('#day3').html(payload.list["16"].dt_txt);
+    $('#day3').html(payload.list["16"].dt_txt.slice(5,10)+'-2018');
     $('#day3-temp').html(payload.list["16"].main.temp + '°');
     $('#day3-icon').html('<img src="http://openweathermap.org/img/w/'+ payload.list["16"].weather["0"].icon + '.png">');
-    $('#day4').html(payload.list["24"].dt_txt);
+    $('#day4').html(payload.list["24"].dt_txt.slice(5,10)+'-2018');
     $('#day4-temp').html(payload.list["24"].main.temp + '°');
     $('#day4-icon').html('<img src="http://openweathermap.org/img/w/'+ payload.list["24"].weather["0"].icon + '.png">');
-    $('#day5').html(payload.list["32"].dt_txt);
+    $('#day5').html(payload.list["32"].dt_txt.slice(5,10)+'-2018');
     $('#day5-temp').html(payload.list["32"].main.temp + '°');
     $('#day5-icon').html('<img src="http://openweathermap.org/img/w/'+ payload.list["32"].weather["0"].icon + '.png">')
   })
