96fe248d7bbcf55ae5179f969b96a01f2d255a23
Haley Townsend <haleyrk@hotmail.com>
2018-02-21 13:13:51 -0600

    add 'city'

--- front-end/index.js
+++ front-end/index.js
@@ -6,7 +6,7 @@ owm.getWeather({city: city})
 owm.getForecast({city:city})
 
 $('#searchBtn').click(function () {
-  city = $('#searchCity').val()
+  var city = $('#searchCity').val
   console.log(city);
   $('#city').html(city);
   owm.getWeather({city: city})
